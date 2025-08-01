ace2d9a4b43f6906fa1228ab0eb3bcd60f22ac80
JAYDEEP SAHU <jaydeepsahu971@gmail.com>
2025-08-01 08:47:00 +0530

    remove v config

--- v_config/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer>
-      <p>© 2025 Vehicle Configurator - VITA C-DAC Project</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- v_config/src/pages/Registration.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Registration() {
-  return <h1>Registration Page</h1>;
-}
-
-export default Registration;
